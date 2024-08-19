e4948dbc49ef4de564a80ba7820fcc6683f80ce5
Trunk Mortise <trunk_mortise_0g@icloud.com>
2024-08-19 16:56:39 +0800

    added popular posts with skeleton loading with swr

--- src/components/home/popular-posts.tsx
+++ src/components/home/popular-posts.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import { popularPosts } from "@/lib/placeholder-data";
-// import { fetcher, fetchUrl } from "@/lib/utils";
+// import { popularPosts } from "@/lib/placeholder-data";
+import { fetcher, fetchUrl } from "@/lib/utils";
 import { Icons } from "../icons";
 import Link from "next/link";
-// import useSWR from "swr";
-// import SkeletonCard from "@/components/skeleton/popular_posts_skeleton";
+import useSWR from "swr";
+import SkeletonCard from "@/components/skeleton/popular_posts_skeleton";
 
 export default function PopularPosts() {
-  // const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
+  const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
 
-  // if (error) return <div>Failed to load</div>;
-  // if (isLoading) return <SkeletonCard />;
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <SkeletonCard />;
 
   return (
-    
     <ul className="overflow-auto">
-      {popularPosts.map((post) => (
-          <Link href={`/blog/`} key={post.title}>
+      {data?.map((post: { category: string; slug: string; title: string }) => (
+        <Link href={`/blog/${post.category}/${post.slug}`} key={post.title}>
           <li className="flex items-center gap-2 group cursor-pointer py-2">
             <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all" />
             <p>{post.title}</p>
@@ -25,16 +24,5 @@ export default function PopularPosts() {
         </Link>
       ))}
     </ul>
-    
-    // <ul className="overflow-auto">
-    //   {data?.map((post: { category: string; slug: string; title: string }) => (
-    //     <Link href={`/blog/${post.category}/${post.slug}`} key={post.title}>
-    //       <li className="flex items-center gap-2 group cursor-pointer py-2">
-    //         <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all" />
-    //         <p>{post.title}</p>
-    //       </li>
-    //     </Link>
-    //   ))}
-    // </ul>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,17 +10,12 @@ export const fetchUrl =
     ? "http://localhost:3000/api"
     : "https://codemasters-psi.vercel.app/api";
 
-// export const fetchUrl =
-//   process.env.NODE_ENV === "development"
-//     ? "http://localhost:3000/api"
-//     : "https://next-blog-cj.vercel.app/api";
+type ResponseData = {
+  category: string;
+  title: string;
+  slug: string;
+}[];
 
-// type ResponseData = {
-//   category: string;
-//   title: string;
-//   slug: string;
-// }[];
-
-// export const fetcher = (
-//   ...args: Parameters<typeof fetch>
-// ): Promise<ResponseData> => fetch(...args).then((res) => res.json());
+export const fetcher = (
+  ...args: Parameters<typeof fetch>
+): Promise<ResponseData> => fetch(...args).then((res) => res.json());
